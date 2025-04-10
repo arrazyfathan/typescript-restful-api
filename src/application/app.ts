e378261fcb6b8b8f9ec8008ce5646551dd6d256a
arrazyfathan <arrazy.rabbani266@gmail.com>
2025-04-10 09:09:45 +0700

    feat: add user login api

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,8 +1,14 @@
-import express from 'express';
-import {publicRouter} from "../route/public-api";
-import {errorMiddleware} from "../middleware/error-middleware";
+import express from "express";
+import { publicRouter } from "../route/public-api";
+import { errorMiddleware } from "../middleware/error-middleware";
+import {logger} from "./logging";
 
 export const app = express();
 app.use(express.json());
 app.use(publicRouter);
-app.use(errorMiddleware)
+app.use(errorMiddleware);
+
+
+app.listen(3000, () => {
+    logger.info(`Server is running on port http://localhost:3000`);
+})
--- test/test-util.ts
+++ test/test-util.ts
@@ -1,4 +1,5 @@
 import {prismaClient} from "../src/application/database";
+import bcrypt from "bcrypt";
 
 export class UserTest {
 
@@ -8,5 +9,16 @@ export class UserTest {
                 username: "test"
             }
         })
+    }
+
+    static async create() {
+        await prismaClient.user.create({
+            data: {
+                username: "test",
+                name: "test",
+                password: await bcrypt.hash("secret", 10),
+                token: "test"
+            }
+        });
     }
 }
