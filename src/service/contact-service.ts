9b9b8d240863d731a1b005c79f891af6e6061f69
arrazyfathan <arrazy.rabbani266@gmail.com>
2025-04-11 09:15:15 +0700

    feat : add create contact api

---
+++ src/service/contact-service.ts
@@ -0,0 +1,21 @@
+import {ContactResponse, CreateContactRequest, toContactResponse} from "../model/contact-model";
+import {Validation} from "../validation/validation";
+import {ContactValidation} from "../validation/contact-validation";
+import {User} from "@prisma/client";
+import {prismaClient} from "../application/database";
+
+export class ContactService {
+
+    static async create(user: User, request: CreateContactRequest): Promise<ContactResponse> {
+        const createRequest = Validation.validate(ContactValidation.CREATE, request);
+
+        const contact = await prismaClient.contact.create({
+            data: {
+                ...createRequest,
+                username: user.username,
+            }
+        })
+
+        return toContactResponse(contact);
+    }
+}
