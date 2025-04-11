77a2a7f52cf7e2c12da3c8f236eade6ba17c160e
arrazyfathan <arrazy.rabbani266@gmail.com>
2025-04-11 09:43:10 +0700

    feat : add get contact by id api

--- src/service/contact-service.ts
+++ src/service/contact-service.ts
@@ -3,6 +3,7 @@ import {Validation} from "../validation/validation";
 import {ContactValidation} from "../validation/contact-validation";
 import {User} from "@prisma/client";
 import {prismaClient} from "../application/database";
+import {ResponseError} from "../error/response-error";
 
 export class ContactService {
 
@@ -18,5 +19,20 @@ export class ContactService {
         })
 
         return toContactResponse(contact);
+    }
+
+    static async get(user: User, id: number): Promise<ContactResponse> {
+        const contact = await prismaClient.contact.findUnique({
+            where: {
+                id: id,
+                username: user.username
+            }
+        })
+
+        if (!contact) {
+            throw new ResponseError(404, "Contact not found");
+        }
+
+        return toContactResponse(contact)
     }
 }
